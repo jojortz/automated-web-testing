5a1a3064bd35189261f4be8301c0f405829f59bc
jojortz <ortiz.jroman@gmail.com>
2025-03-06 11:13:41 -0800

    retrive login token

---
+++ api-calls/getLoginToken.js
@@ -0,0 +1,17 @@
+import * as nodeFetch from "node-fetch";
+
+export const getLoginToken = async () => {
+    const response = await nodeFetch('http://localhost:2221/api/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: 'admin',
+            password: 'Admin123'
+        })
+    });
+
+    if (response.status !== 200) {
+        throw new Error('Failed to get login token');
+    }
+    const body = await response.json();
+    return body.token;
+}
--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -1,7 +1,12 @@
 import { test } from "@playwright/test";
 import { MyAccountPage } from "../page-objects/MyAccountPage";
+import { getLoginToken } from "../api-calls/getLoginToken";
 
 test.only("My Account using cookie injection", async ({ page }) => {
+
+    const loginToken = await getLoginToken();
+    console.warn({loginToken});
+
     const myAccount = new MyAccountPage(page);
     await myAccount.visit();
 });
